36beaf83f00ca5f751ad29dc41c1fb6d299e2691
Johan <johan.zuluaga@utp.edu.co>
2024-08-02 11:50:17 -0500

    added text in main image

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,7 +4,13 @@ import ContainerProduct from "./components/containerProduct/containerProduct";
 export default function HomePage() {
   return (
     <div className="p-5 pt-24 flex flex-col flex-wrap justify-center align-middle gap-5">
-      <img src="/img/hamburgerMain.jpg" alt="Hamburger Icon" />
+      <div className="relative">
+        <img src="/img/hamburgerMain.jpg" alt="Hamburger Icon" />
+        <p className="absolute bottom-0 w-full text-center text-white p-2 text-2xl">
+          ¡Hey, qué pasa! Pide tu comida rápida favorita y a disfrutar.
+        </p>
+      </div>
+
       <CarruselCategories />
       <ContainerProduct category="Hamburguesas" />
     </div>
