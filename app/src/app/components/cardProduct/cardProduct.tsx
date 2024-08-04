5944fcef00ae99dd594cb5dd4829f13877207691
Juan Jose Ruiz Mellizo <Juanjose.ruiz1@utp.edu.co>
2024-08-04 16:21:44 -0500

    animation base

--- app/src/app/components/cardProduct/cardProduct.tsx
+++ app/src/app/components/cardProduct/cardProduct.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import "./cardProduct.css";
+import { motion, useAnimation } from 'framer-motion';
 import Image from "next/image";
 import { useState } from "react";
 import DetailsCard from "../detailsCard/detailsCard";
-
+import "./cardProduct.css";
 
 interface Product {
   id: string;
@@ -18,6 +18,32 @@ interface CardProductProps {
 
 const CardProduct: React.FC<CardProductProps> = ({ product }) => {
   const [isModalVisible, setModalVisible] = useState(false);
+  const [isCardFlipped, setCardFlipped] = useState(false);
+  const controls = useAnimation();
+
+  const handleOpenModal = () => {
+    // ...
+    controls.start({
+      rotateY: 180,
+      scale: 1.2,
+      opacity: 0,
+      transition: { duration: 1 },
+    }).then(() => {
+      setModalVisible(true);
+    });
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    controls.start({
+      rotateY: 0,
+      scale: 1,
+      opacity: 1, // Restablece la opacidad a 1 para que sea visible
+      transition: { duration: 0.5 },
+    }).then(() => {
+      setCardFlipped(false);
+    });
+  };
 
   const formatPrice = (price: number) => {
     return new Intl.NumberFormat("es-ES", {
@@ -25,26 +51,24 @@ const CardProduct: React.FC<CardProductProps> = ({ product }) => {
     }).format(price);
   };
 
-  const handleOpenModal = () => {
-    console.log("Opening modal");
-    setModalVisible(true);
-  };
-  
-  const handleCloseModal = () => {
-    console.log("Closing modal");
-    setModalVisible(false);
-  };
-
-
   return (
-    <div className="detailsProduct" onClick={handleOpenModal}>
-      <Image src="/img/hamburgerMain.jpg" alt={product.name} width={200} height={200} priority={true} style={{width: "200",height: "auto"}}/>
-      <h3 className="titleProduct font-Lacquer">
-        {product.name.toLowerCase()}
-      </h3>
-      <p className="price font-LexendExa">${formatPrice(product.price)}</p>
+    <>
+      <motion.div
+        className={`detailsProduct ${isCardFlipped ? 'flipped' : ''}`}
+        onClick={handleOpenModal}
+        initial={{ scale: 1 }}
+        animate={controls}
+        style={{ originX: 0.5, originY: 0.5 }}
+      >
+        <Image src="/img/hamburgerMain.jpg" alt={product.name} width={200} height={200} priority={true} style={{ width: "200px", height: "auto" }} />
+        <h3 className="titleProduct font-Lacquer">
+          {product.name.toLowerCase()}
+        </h3>
+        <p className="price font-LexendExa">${formatPrice(product.price)}</p>
+      </motion.div>
+
       <DetailsCard product={product} isVisible={isModalVisible} onClose={handleCloseModal} />
-    </div>
+    </>
   );
 };
 
--- app/src/app/components/detailsCard/detailsCard.tsx
+++ app/src/app/components/detailsCard/detailsCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FC, useState, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
 import './detailsCard.css';
 import SwitchGroup from './addingSwitch/addingSwitch';
@@ -19,17 +20,36 @@ interface DetailsCardProps {
 
 const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
   const [isInfoVisible, setInfoVisible] = useState(false);
+  const controls = useAnimation();
+  const [showOverlay, setShowOverlay] = useState(false);
 
   useEffect(() => {
     if (isVisible) {
       document.body.classList.add('no-scroll');
+      controls.start({
+        opacity: 1,
+        scale: 1,
+        transition: {
+          delay: 0.05, // Reduce el delay a 0.25 segundos
+          duration: 0.5, // Ajusta la duración si es necesario
+        },
+      }).then(() => {
+        setShowOverlay(true);
+      });
     } else {
       document.body.classList.remove('no-scroll');
+      controls.start({
+        opacity: 0,
+        scale: 1,
+        rotateY: 0, // Asegúrate de que la rotación sea 0 al cerrar
+        transition: { duration: 0.5 },
+      });
+      setShowOverlay(false);
     }
     return () => {
       document.body.classList.remove('no-scroll');
     };
-  }, [isVisible]);
+  }, [isVisible, controls]);
 
   if (!isVisible) return null;
 
@@ -40,16 +60,16 @@ const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
   };
 
   const handleCloseClick = (event: React.MouseEvent) => {
-    event.stopPropagation(); // Evita que el clic se propague al contenedor del producto
+    event.stopPropagation();
     onClose();
   };
 
   const handleInfoClick = () => {
-    setInfoVisible(true); // Muestra la información
+    setInfoVisible(true);
   };
 
   const handleHideInfo = () => {
-    setInfoVisible(false); // Oculta la información
+    setInfoVisible(false);
   };
 
   const switchItems = [
@@ -59,9 +79,21 @@ const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
   ];
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-start pt-24"> {/* Ajuste aquí */}
+    <motion.div
+      className="fixed inset-0 flex justify-center items-center"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={controls}
+      transition={{ duration: 0.5 }}
+    >
+      {showOverlay && (
+        <motion.div
+          className="fixed inset-0 bg-black bg-opacity-50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.2 }}
+        />
+      )}
       <div className="relative p-6 rounded-3xl shadow-lg mx-4 md:mx-6 detailsCard border-2">
-        {/* Título y botón de cierre de la tarjeta de detalles */}
         <div className='mb-4 flex justify-between border-b-2 border-white pb-2'>
           <h2 className="text-2xl mx-1 text-white flex items-center justify-center font-Lacquer">{product.name.toLowerCase()}</h2>
           <button className="mx-3 text-2xl mb-3" onClick={handleCloseClick}>
@@ -74,17 +106,15 @@ const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
           </button>
         </div>
 
-        {/* Imagen de fondo */}
         <div className="relative flex justify-center items-center infoBox ">
           <Image
             src="/img/hamburger.jpg"
             alt='Hamburger'
             className='rounded-2xl'
-            width={300} // Ancho deseado
-            height={200} // Altura deseada
+            width={300}
+            height={200}
             priority={true}
           />
-          {/* Texto informativo */}
           {isInfoVisible && (
             <div className='absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 rounded-2xl font-LYLAS'>
               <p className='Info text-white p-4 rounded-lg'>
@@ -92,7 +122,6 @@ const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
               </p>
             </div>
           )}
-          {/* Ícono de información */}
           {!isInfoVisible && (
             <Image
               src="/info.svg"
@@ -103,7 +132,6 @@ const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
               onClick={handleInfoClick}
             />
           )}
-          {/* Botón de cierre del texto informativo */}
           {isInfoVisible && (
             <button
               className="absolute top-2 left-2"
@@ -119,7 +147,6 @@ const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
           )}
         </div>
         <SwitchGroup items={switchItems} />
-        {/* Precio del producto */}
         <div className='flex items-center justify-center'>
           <p className=" text-2xl mt-2 mb-2 p-4 rounded-tl-xl rounded-bl-xl priceContainer buyElement text-center font-LexendExa">${formatPrice(product.price)}</p>
           <button className="text-2xl mt-2 mb-2 p-2 rounded-tr-xl rounded-br-xl addContainer buyElement flex items-center justify-center" onClick={handleCloseClick}>
@@ -132,7 +159,7 @@ const DetailsCard: FC<DetailsCardProps> = ({ product, isVisible, onClose }) => {
           </button>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
